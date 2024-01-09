a7f03f9b3d74e49c10a508fde2a8a2215b145436
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-09 18:47:13 +0000

    added some dummy data in the controller to do a quick render test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Render } from '@nestjs/common/decorators';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('login')
+  getHello(): { "message": string } {
+    return { "message": "Login Data" }
   }
 }
